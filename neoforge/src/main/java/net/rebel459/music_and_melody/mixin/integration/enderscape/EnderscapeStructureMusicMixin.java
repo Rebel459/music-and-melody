119caf82e5379b6277f47e16d534c53875fb83f7
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-05-22 14:14:40 +1000

    1.21.1 enderscape integration

---
+++ neoforge/src/main/java/net/rebel459/music_and_melody/mixin/integration/enderscape/EnderscapeStructureMusicMixin.java
@@ -0,0 +1,22 @@
+package net.rebel459.music_and_melody.mixin.integration.enderscape;
+
+import net.bunten.enderscape.client.EnderscapeClient;
+import net.bunten.enderscape.client.registry.EnderscapeClientNetworking;
+import net.bunten.enderscape.network.ClientboundStructureChangedPayload;
+import net.neoforged.neoforge.network.handling.IPayloadContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+
+@Mixin(EnderscapeClientNetworking.class)
+public class EnderscapeStructureMusicMixin {
+
+    @Inject(method = "receiveStructureChangedPayload", at = @At(value = "HEAD"), cancellable = true)
+    private static void disableEnderscapeStructureMusic(ClientboundStructureChangedPayload payload, IPayloadContext context, CallbackInfo ci) {
+        EnderscapeClient.structureMusic = Optional.empty();
+        ci.cancel();
+    }
+}
