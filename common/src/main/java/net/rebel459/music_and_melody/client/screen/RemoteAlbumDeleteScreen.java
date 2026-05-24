63f8aee9bf6d8cc93d53b3d4bd8920c09256c5a7
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-05-24 22:58:48 +1000

    Update RemoteAlbumDeleteScreen.java

--- common/src/main/java/net/rebel459/music_and_melody/client/screen/RemoteAlbumDeleteScreen.java
+++ common/src/main/java/net/rebel459/music_and_melody/client/screen/RemoteAlbumDeleteScreen.java
@@ -157,7 +157,7 @@ public class RemoteAlbumDeleteScreen extends Screen {
 
             graphics.text(
                     this.minecraft.font,
-                    Component.literal(id).withStyle(ChatFormatting.GRAY),
+                    Component.literal(id + " | " + this.pack.version()).withStyle(ChatFormatting.GRAY),
                     this.getContentX() + 1,
                     this.getContentYMiddle() + 2,
                     0xFFAAAAAA
