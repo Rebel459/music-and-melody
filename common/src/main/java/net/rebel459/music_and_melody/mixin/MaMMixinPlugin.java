b6cd5969ecca07b3f0b50ba6ec3fd2e02733ef95
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-05-26 11:49:53 +1000

    neo simple music detection

--- common/src/main/java/net/rebel459/music_and_melody/mixin/MaMMixinPlugin.java
+++ common/src/main/java/net/rebel459/music_and_melody/mixin/MaMMixinPlugin.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Set;
 
 public final class MaMMixinPlugin implements IMixinConfigPlugin {
-    public static final boolean CURSEFORGE_DISTRIBUTION = hasClass("net.rebel459.music_and_melody.CurseForgeDistribution");
 
     private static boolean classExists(String className) {
         try {
@@ -32,19 +31,10 @@ public final class MaMMixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, @NotNull String mixinClassName) {
-        if (mixinClassName.contains("integration.simple_music_control.")) return classExists("me.pajic.simple_music_control.Main");
+        if (mixinClassName.contains("integration.simple_music_control.")) return classExists("me.pajic.simple_music_control.Main") || classExists("me.pajic.simple_music_control.ClientMain");
         if (mixinClassName.contains("integration.enderscape.")) return classExists("net.bunten.enderscape.Enderscape");
         if (mixinClassName.contains("integration.fancymenu.")) return classExists("de.keksuccino.fancymenu.FancyMenu");
         return true;
-    }
-
-    private static boolean hasClass(String className) {
-        try {
-            Class.forName(className, false, MaMMixinPlugin.class.getClassLoader());
-            return true;
-        } catch (ClassNotFoundException exception) {
-            return false;
-        }
     }
 
     @Override
