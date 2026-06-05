9a3d157dc406409adaddc1924b10651312800544
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-06-06 01:31:27 +1000

    album disabling fix + sound event registry id rewrite

--- common/src/main/java/net/rebel459/music_and_melody/sound/MaMSounds.java
+++ common/src/main/java/net/rebel459/music_and_melody/sound/MaMSounds.java
@@ -1,6 +1,7 @@
 package net.rebel459.music_and_melody.sound;
 
 import net.minecraft.core.Holder;
+import net.minecraft.resources.Identifier;
 import net.minecraft.sounds.SoundEvent;
 import net.rebel459.music_and_melody.config.MaMServerConfig;
 import net.rebel459.music_and_melody.platform.MaMPlatform;
@@ -11,8 +12,8 @@ import java.util.Set;
 
 public class MaMSounds {
 
-	public static HashMap<String, Holder<SoundEvent>> REGISTERED_SOUNDS = new HashMap<>();
-	
+	public static HashMap<String, Holder<SoundEvent>> BUILT_IN_SOUNDS = new HashMap<>();
+
 	public static void init() {
 		MaMPlatform.SOUND_EVENTS.registerVanilla("music_disc.bounce");
 		Set<String> sounds = new HashSet<>(MaMServerConfig.get().sound_events);
@@ -28,10 +29,13 @@ public class MaMSounds {
 				"music.wither",
 				"music.threshold"
 		);
-		sounds.addAll(builtInSounds);
-		for (String pool : sounds) {
+		for (String pool : builtInSounds) {
 			Holder<SoundEvent> sound = MaMPlatform.SOUND_EVENTS.registerForHolder(pool);
-			REGISTERED_SOUNDS.put(pool, sound);
+			BUILT_IN_SOUNDS.put(pool, sound);
+		}
+		for (String pool : sounds) {
+			Identifier id = Identifier.parse(pool);
+			UnifiedRegistries.SoundEvents.create(id.getNamespace()).registerForHolder(id.getPath());
 		}
 	}
 }
--- neoforge/src/main/java/net/rebel459/music_and_melody/MusicAndMelodyNeoForge.java
+++ neoforge/src/main/java/net/rebel459/music_and_melody/MusicAndMelodyNeoForge.java
@@ -5,8 +5,13 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.rebel459.music_and_melody.platform.MaMNeoForgePlatform;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Mod(MusicAndMelody.MOD_ID)
 public class MusicAndMelodyNeoForge {
+
+    private static final Set<String> registeredBuses = new HashSet<>(Set.of(MusicAndMelody.MOD_ID, "minecraft"));
 
     public MusicAndMelodyNeoForge(IEventBus modEventBus) {
         MaMNeoForgePlatform.init(modEventBus);
