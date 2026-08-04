f7472bcbdc48b2fc7fcda726fb0c2ce5447ffa38
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-08-04 20:32:47 +1000

    update enderscape integration

--- fabric/src/main/java/net/rebel459/music_and_melody/mixin/integration/enderscape/EnderscapeClientNetworkingMixin.java
+++ fabric/src/main/java/net/rebel459/music_and_melody/mixin/integration/enderscape/EnderscapeClientNetworkingMixin.java
@@ -1,9 +1,9 @@
 package net.rebel459.music_and_melody.mixin.integration.enderscape;
 
-import net.bunten.enderscape.client.EnderscapeClient;
-import net.bunten.enderscape.client.registry.EnderscapeClientNetworking;
-import net.bunten.enderscape.network.ClientboundStructureChangedPayload;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.penumbra.enderscape.EnderscapeClient;
+import net.penumbra.enderscape.network.ClientboundStructureChangedPayload;
+import net.penumbra.enderscape.registry.networking.EnderscapeClientNetworking;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,11 +12,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Optional;
 
 @Mixin(EnderscapeClientNetworking.class)
-public class EnderscapeStructureMusicMixin {
+public class EnderscapeClientNetworkingMixin {
 
     @Inject(method = "receiveStructureChangedPayload", at = @At(value = "HEAD"), cancellable = true)
     private static void disableEnderscapeStructureMusic(ClientboundStructureChangedPayload payload, ClientPlayNetworking.Context context, CallbackInfo ci) {
-        EnderscapeClient.structureMusic = Optional.empty();
+        EnderscapeClient.clientsideVariables().structureMusic = Optional.empty();
         ci.cancel();
     }
 }
