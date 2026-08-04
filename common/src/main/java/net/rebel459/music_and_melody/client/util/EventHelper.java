404a539c89d5fa37e516d28682ddc216204c3f38
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-08-04 20:12:19 +1000

    Update EventHelper.java

--- common/src/main/java/net/rebel459/music_and_melody/client/util/EventHelper.java
+++ common/src/main/java/net/rebel459/music_and_melody/client/util/EventHelper.java
@@ -46,7 +46,13 @@ public class EventHelper {
 
     public static boolean isEndPortalFilled() {
         SoundManager manager = Minecraft.getInstance().getSoundManager();
-        return manager.soundEngine.instanceBySource.values().stream().filter(instance -> SoundEvents.END_PORTAL_SPAWN.getLocation().equals(instance.getLocation())).anyMatch(manager::isActive);
+        Collection<SoundInstance> blockSounds = manager.soundEngine.instanceBySource.get(SoundSource.BLOCKS);
+        for (SoundInstance instance : new ArrayList<>(blockSounds)) {
+            if (SoundEvents.END_PORTAL_SPAWN.getLocation().equals(instance.getLocation()) && manager.isActive(instance)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Music processEventMusic() {
