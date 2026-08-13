18cd245cda9db939e2ed6d1c1fbec149289527e6
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-08-13 15:11:48 +1000

    improve remote download reload performance

--- common/src/main/java/net/rebel459/music_and_melody/mixin/client/MultiPackResourceManagerMixin.java
+++ common/src/main/java/net/rebel459/music_and_melody/mixin/client/MultiPackResourceManagerMixin.java
@@ -7,6 +7,7 @@ import net.rebel459.music_and_melody.client.remote.DownloadedResources;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
@@ -19,6 +20,11 @@ import java.util.function.Predicate;
 
 @Mixin(MultiPackResourceManager.class)
 public class MultiPackResourceManagerMixin {
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void invalidateDownloadedResources(CallbackInfo ci) {
+        DownloadedResources.invalidate();
+    }
 
     @Inject(method = "getNamespaces", at = @At("RETURN"), cancellable = true)
     private void addDownloadedNamespaces(CallbackInfoReturnable<Set<String>> cir) {
